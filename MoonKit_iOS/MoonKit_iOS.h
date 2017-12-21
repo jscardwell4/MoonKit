39d030702c7957de7851fef3e1b5d43e6b2b7744
Jason Cardwell <cardwell.jason@gmail.com>
2017-12-21 06:59:21 -0800

    Initial Commit

---
+++ MoonKit_iOS/MoonKit_iOS.h
@@ -0,0 +1,19 @@
+//
+//  MoonKit_iOS.h
+//  MoonKit_iOS
+//
+//  Created by Jason Cardwell on 12/21/17.
+//  Copyright © 2017 Moondeer Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MoonKit_iOS.
+FOUNDATION_EXPORT double MoonKit_iOSVersionNumber;
+
+//! Project version string for MoonKit_iOS.
+FOUNDATION_EXPORT const unsigned char MoonKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoonKit_iOS/PublicHeader.h>
+
+
